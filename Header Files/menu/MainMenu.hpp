b1ca8f775ff6bba9f815033d95947e67b46baee2
Rozalia <rozalia.ekiert@gmail.com>
2023-12-29 19:02:02 +0100

    refactor

--- Header Files/menu/MainMenu.hpp
+++ Header Files/menu/MainMenu.hpp
@@ -1,6 +1,21 @@
 #pragma once
+#include "../stdafx.hpp"
 
 class MainMenu {
+public:
+    MainMenu(float width, float height);
+
+    void draw(sf::RenderWindow & window);
+
+    int MainMenuPressed(){
+        return MainMenuSelected;
+    }
+
+    ~MainMenu();
+
+private:
+    int MainMenuSelected;
+    sf::Font font;
 
 };
 
--- Source Files/menu/MainMenu.cpp
+++ Source Files/menu/MainMenu.cpp
@@ -1,1 +1,7 @@
 #include "../../Header Files/menu/MainMenu.hpp"
+
+MainMenu::MainMenu(float width, float height) {
+    if (!font.loadFromFile("../assets/font/Planes_ValMore.ttf")){
+        std::cout << "ERROR: Could not load font from file\n";
+    }
+}
